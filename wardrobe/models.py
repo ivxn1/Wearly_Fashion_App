39f6b472fb4bd0bf6a3fae7cafba3c6c461acc22
Ivan Zhelev <ivxn.zhelev@gmail.com>
2026-01-14 14:31:30 +0200

    Updated verbose name plural of Category model in app wardrobe

--- wardrobe/models.py
+++ wardrobe/models.py
@@ -32,11 +32,9 @@ class Category(models.Model):
         blank=False,
         null=False
     )
-    slug = models.TextField(
-        unique=True,
-        blank=True,
-        null=True
-    )
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
