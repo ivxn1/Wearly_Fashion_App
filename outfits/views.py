ec5115e0c0b0d1b4417fd9649ee213235e904fd5
Ivan Zhelev <ivxn.zhelev@gmail.com>
2026-01-20 21:38:13 +0200

    Created outfits list html and css

--- outfits/views.py
+++ outfits/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from outfits.models import Outfit
+
+
 # Create your views here.
+def outfits_list(request: HttpRequest) -> HttpResponse:
+    outfits = Outfit.objects.select_related('occasion').prefetch_related('garments')
+
+    context = {
+        'page_title': 'Wearly Outfits',
+        'outfits': outfits,
+    }
+
+    return render(request, 'outfits/outfits_list.html', context)
