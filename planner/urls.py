8729d1ad8908e8420618cd79b6153ccf6efc169e
Ivan Delchev Zhelev <ivanjelev2004@gmail.com>
2026-01-25 21:39:39 +0200

    Feature/plan entry details page (#24)
    * Created plan_entry_details view and its url
    
    * Created plan_entry_details HTML and CSS and fixed url to this page in plan_list page
    
    ---------
    
    Co-authored-by: Ivan Zhelev <ivxn.zhelev@gmail.com>

--- planner/urls.py
+++ planner/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from planner.views import planner_list
+from planner.views import planner_list, plan_details
 
 app_name = 'planner'
 
 urlpatterns = [
-    path('planner/', planner_list, name='list')
+    path('planner/', planner_list, name='list'),
+    path('planner/<int:pk>/', plan_details, name='plan_details'),
 ]
--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from planner.models import PlanEntry
 # Create your views here.
@@ -7,3 +8,17 @@ app_name = 'planner'
 def planner_list(request):
     plans = PlanEntry.objects.prefetch_related('outfit').order_by('date')
     return render(request, 'planner/planner_list.html', {'plans': plans, 'page_title': 'Wearly Planner'})
+
+def plan_details(request: HttpRequest, pk:int) -> HttpResponse:
+    plan_entry = PlanEntry.objects.prefetch_related('outfit').get(pk=pk)
+    prev_plan = PlanEntry.objects.filter(date__lt=plan_entry.date).order_by('-date').first()
+    next_plan = PlanEntry.objects.filter(date__gt=plan_entry.date).order_by('date').first()
+
+    context = {
+        'page_title': f'Plan for {plan_entry.date}',
+        'plan_entry': plan_entry,
+        'prev_plan': prev_plan,
+        'next_plan': next_plan,
+    }
+
+    return render(request, 'planner/plan_entry_details.html', context)
