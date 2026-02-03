0c230c9ed635fd67bc1163643cd28f57bd9178a3
Ivan Zhelev <ivxn.zhelev@gmail.com>
2026-02-03 15:36:47 +0200

    Established views and urls for each outfit form

--- outfits/urls.py
+++ outfits/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 
-from outfits.views import outfits_list, outfit_details
+from outfits.views import outfits_list, outfit_details, add_outfit, edit_outfit, confirm_delete_outfit
 
 app_name = 'outfits'
 
 urlpatterns = [
-    path('outfits/', outfits_list, name='outfits_list'),
-    path('outfits/<int:id>/', outfit_details, name='outfit_details'),
+    path('outfits/', include([
+        path('', outfits_list, name='outfits_list'),
+        path('add/', add_outfit, name='add_outfit'),
+        path('<int:id>/', include([
+            path('', outfit_details, name='outfit_details'),
+            path('edit/', edit_outfit, name='edit_outfit'),
+            path('delete/', confirm_delete_outfit, name='confirm_delete_outfit'),
+    ])),
+    ])
+         ),
 ]
--- outfits/views.py
+++ outfits/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from outfits.forms import OutfitCreateForm
 from outfits.models import Outfit
 
 
 # Create your views here.
 def outfits_list(request: HttpRequest) -> HttpResponse:
-    outfits = Outfit.objects.select_related('occasion').prefetch_related('garments')
+    outfits = Outfit.objects.prefetch_related('garments')
 
     context = {
         'page_title': 'Wearly Outfits',
@@ -26,3 +27,46 @@ def outfit_details(request: HttpRequest, id:int) -> HttpResponse:
     }
 
     return render(request, 'outfits/outfit_details.html', context)
+
+def add_outfit(request: HttpRequest) -> HttpResponse:
+    form = OutfitCreateForm(request.POST or None, request.FILES or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect('outfits:outfit_details', id=form.instance.id)
+
+    context = {
+        'page_title': 'Add Outfit',
+        'form': form,
+    }
+
+    return render(request, 'outfits/outfit_add_form.html', context)
+
+def edit_outfit(request: HttpRequest, id:int) -> HttpResponse:
+    outfit = get_object_or_404(Outfit, id=id)
+    form = OutfitCreateForm(request.POST or None, instance=outfit, files=request.FILES or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect('outfits:outfit_details', id=form.instance.id)
+
+    context = {
+        'page_title': f'Edit {outfit.title}',
+        'form': form,
+        'outfit': outfit,
+    }
+
+    return render(request, 'outfits/outfit_edit_form.html', context)
+
+def confirm_delete_outfit(request: HttpRequest, id:int) -> HttpResponse:
+    outfit = get_object_or_404(Outfit, id=id)
+    if request.method == "POST":
+        outfit.delete()
+        return redirect('outfits:outfits_list')
+
+    context = {
+        'page_title': f'Delete {outfit.title}',
+        'outfit': outfit,
+    }
+
+    return render(request, 'outfits/outfit_confirm_delete.html', context)
