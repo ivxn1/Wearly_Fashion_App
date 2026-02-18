2efc63590f1cd589e12307fb9fa83343f4977913
Ivan Zhelev <ivxn.zhelev@gmail.com>
2026-02-18 13:49:30 +0200

    Moved SeasonChoices from outfits and wardrobe app to core app to prevent redundancy

---
+++ core/choices.py
@@ -0,0 +1,22 @@
+"""
+Shared choice definitions for the Wearly application.
+
+This module contains TextChoices enums that are used across
+multiple apps in the project.
+"""
+
+from django.db import models
+
+
+class SeasonChoices(models.TextChoices):
+    """
+    Enumeration of season options for garments and outfits.
+
+    Includes an 'All Seasons' option for items suitable year-round.
+    """
+
+    ALL = "", "All Seasons"
+    SPRING = "spring", "Spring"
+    SUMMER = "summer", "Summer"
+    AUTUMN = "autumn", "Autumn"
+    WINTER = "winter", "Winter"
