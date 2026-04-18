7748d8532740190c72e35eedc85fcd865f866fc7
Ivan Zhelev <ivxn.zhelev@gmail.com>
2026-04-18 19:13:58 +0300

    Updated env vars and gitignore

--- Wearly_Fashion_App/settings.py
+++ Wearly_Fashion_App/settings.py
@@ -150,12 +150,12 @@ CELERY_BEAT_SCHEDULE = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ivxn.zhelev@gmail.com'
-EMAIL_HOST_PASSWORD = 'tbyoxmquqviuryig'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
