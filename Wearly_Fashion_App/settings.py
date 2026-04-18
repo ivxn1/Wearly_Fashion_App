d6c79869698cf027843cae80d3ce9cefbe355816
Ivan Zhelev <ivxn.zhelev@gmail.com>
2026-04-18 21:23:23 +0300

    Added redis url

--- Wearly_Fashion_App/settings.py
+++ Wearly_Fashion_App/settings.py
@@ -142,9 +142,11 @@ LOGIN_URL = "user-login"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "user-login"
 
+REDIS_URL = os.getenv("REDIS_URL")
+
 # Celery Configuration
-CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
-CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
+CELERY_BROKER_URL = REDIS_URL
+CELERY_RESULT_BACKEND = REDIS_URL
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 
