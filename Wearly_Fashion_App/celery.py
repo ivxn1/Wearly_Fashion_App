4c38ab028f96d5dd1b9daecf3e00ddc97db493e6
Ivan Zhelev <ivxn.zhelev@gmail.com>
2026-04-18 16:51:57 +0300

    Configured celery

---
+++ Wearly_Fashion_App/celery.py
@@ -0,0 +1,7 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Wearly_Fashion_App.settings")
+app = Celery("Wearly_Fashion_App")
+app.config_from_object('django.conf:settings', namespace="CELERY")
+app.autodiscover_tasks()
